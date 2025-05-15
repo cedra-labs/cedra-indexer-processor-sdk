58a542e8a7440ba497edae0d240a673d6485c8c5
Inkvisto <qwertuasdfgh953@gmail.com>
2025-05-15 21:39:37 +0300

    fix: minor changes

--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -11,6 +11,7 @@ pub mod types;
 pub mod utils;
 
 // Re-exporting crates to provide a cohesive SDK interface
+pub use cedra_indexer_transaction_stream;
 pub use aptos_protos;
 pub use bcs;
 pub use instrumented_channel;
