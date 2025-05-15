127bf76b35e1e094b19df9a4b9dd6ee1278296ac
Inkvisto <qwertuasdfgh953@gmail.com>
2025-05-15 21:03:51 +0300

    fix: refactord imports

--- sdk/src/postgres/basic_processor/basic_processor_function.rs
+++ sdk/src/postgres/basic_processor/basic_processor_function.rs
@@ -1,6 +1,6 @@
 use super::basic_processor_step::BasicProcessorStep;
+use cedra_indexer_transaction_stream::TransactionStreamConfig,
 use crate::{
-    cedra_indexer_transaction_stream::TransactionStreamConfig,
     builder::ProcessorBuilder,
     common_steps::{
         TransactionStreamStep, VersionTrackerStep, DEFAULT_UPDATE_PROCESSOR_STATUS_SECS,
